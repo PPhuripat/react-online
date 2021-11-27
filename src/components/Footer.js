5b9a3318653c70b96cef8ae28035fa104a28ca19
PPhuripat <bu.phuripat_st@tni.ac.th>
2021-11-27 11:05:09 +0700

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,21 @@
 import React from 'react'
-
-export const Footer = () => {
-
-    const nickname = "Copter"
-
+import PropTypes from 'prop-types';
+const Footer = ({title,website,address,postcode, isOpen}) => {
+    //const nickName = "Copter"
+    //const {title,website,address,postcode} = props;
     return (
         <div>
-            <h1>Phuripat Bungwai {nickname}</h1>
+           <h3>{title} &copy; {new Date().getFullYear()}</h3>
+           <p>{website} {address} {postcode} {isOpen.toString}</p>
         </div>
     )
 }
+Footer.propTypes = {
+    title: PropTypes.string,
+    website: PropTypes.string,
+    address: PropTypes.string,
+    postcode: PropTypes.number,
+    status: PropTypes.bool
+}
+
 export default Footer
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,17 +8,53 @@ const Header = () => {
         return companyName + ".com"
     }
 
+    const isLogin = true;
+
+    const showMe = () => {
+        alert("Hello react")
+    }
+
+    const product = [
+        {id: 1, name: "coke"},
+        {id: 2, name: "Pepsi"}
+    ]
+
     return (
         <div>
-            
-            <h1>Hello {showMessage()} </h1>
-        
-            
+            <h1>Hello {showMassage()}</h1>
+            <br />
+            {/* {
+                isLogin === true ? (
+                    <>
+                    <p>Welcome</p>
+                    <p>Student</p>
+                    </>
+                ) : (
+                    <>
+                        <p>Welcome</p>
+                        <p>Teacher</p>
+                    </>
+                )
+            }
 
-            <hr />
+            {isLogin ? <Logo /> : <p>Unlock</p> } */}
+
+            <button onClick={showMe}>Click me</button>
+
+            <ul>
+                {
+
+                    products.map((product,index) => {
+                        return (<li key={index}>{index+1}{product.name}</li>)
+                    })
+                }
+                </ul>
+
         </div>
     )
+    
 }
+
 
 export default Header
 
