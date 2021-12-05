a7c41e56a3656e20abf9c07e7214c4e69063633c
PPhuripat <bu.phuripat_st@tni.ac.th>
2021-12-05 21:14:19 +0700

    hooks

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Logo from './components/Logo';
-
+import Sidebar from './components/Sidebar';
+import Menu from './components/Menu';
 
 function App() {
   return (
@@ -11,7 +12,10 @@ function App() {
     <Header/>
     <Footer title="TNI" website="www.google.com" 
     address="Bangkok" postcode={10250} isOpen/>
+     <Sidebar></Sidebar>
+    <Menu></Menu>
     </div>
+   
   );
 }
 
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { logo , title } from '../styles/style.js'
-
+import useHoverLogo from "../hooks/useHoverLogo";
 const Logo = () => {
     const logoimages = {
         url: './logo192.png'
 
     }
+    const [hover,mouseOver,mouseOut] = useHoverLogo()
     return (
         <div>
-            <h3 style={title}>Logo</h3>
-            <img style={logo} src={logoimages.url} width="100" alt="logo"></img>
+             {
+                hover ? <h3 style={title}>Logo</h3> : null
+            }
+
+                <img onMouseOver={mouseOver} onMouseOut={mouseOut} style={logo} src={logoimages.url} width="100" alt="Logo"/>
         </div>
     )
 }
