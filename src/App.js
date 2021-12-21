06b7b792be1c469527adbf9752ad993044ff630c
PPhuripat <bu.phuripat_st@tni.ac.th>
2021-12-21 14:47:11 +0700

    assignment1

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
       <Route path='/Product'><ProductPage/></Route>
        <Route path='/Contact'><ContactUs></ContactUs></Route>
        <Route path='/Detail'><DetailPage/></Route>
+       
     </Switch>
     <Footer />
   
--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -1,25 +1,36 @@
-import React from 'react'
-import {Card,Button} from 'react-bootstrap'
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import { BsGithub } from "react-icons/bs";
 
 const ContactUs = () => {
-    return (
-        <div className="container ">
-           <div className="row">
-               <div className="col-md-12 mt-4">
-               <Card>
-  <Card.Header>Contact US</Card.Header>
-  <Card.Body>
-    <Card.Title>Phuripat Bungwai</Card.Title>
-    <Card.Text>
-      You can call me "copter" that is my nickname. Have a nice Day
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-               </div>
-           </div>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <div>
+        <p></p>
+        <Card width="30%">
+          <Card.Header>Contact Us</Card.Header>
+          <Card.Body>
+            <Card.Title>Phuripat Bungwai</Card.Title>
+            <Card.Text>
+             Nickname "Copter"
+              <li className="mt-3">
+                My favorite subject = Japanese , Work = Chef
+              </li>
+              <li className="mt-3">Hobby Basketball</li>
+              <li className="mt-3">ไม่ต้องพูดดดดดดด</li>
+            </Card.Text>
+            <p>For my github visit link below.</p>
+            <Button href="https://github.com/PPhuripat" variant="outline-dark">
+              <BsGithub /> Click here
+            </Button>
+            <p></p>
+            <p>Contact me : bu.phuripat_st@tni.ac.th</p>
+          </Card.Body>
+        </Card>
+        <p></p>
+      </div>
+    </div>
+  );
+};
 
-export default ContactUs
+export default ContactUs;
