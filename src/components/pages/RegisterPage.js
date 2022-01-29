c19a6b7112922fd759b89615c389728451824c43
PPhuripat <bu.phuripat_st@tni.ac.th>
2022-01-29 10:37:16 +0700

    7-1

--- src/components/pages/RegisterPage.js
+++ src/components/pages/RegisterPage.js
@@ -4,9 +4,7 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
 import axios from "axios";
-import {
-    useHistory
-  } from "react-router-dom";
+import {useHistory} from "react-router-dom";
 import { useToasts } from 'react-toast-notifications';
 
 
